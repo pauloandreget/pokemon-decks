4b293bba54ceecc548b7e4b0da72d18ac9cee93d
Paulo Rodrigues <pauloandreget@gmail.com>
2020-07-31 18:00:08 -0300

    Setup configuration files

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const webpack = require('webpack');
+const { join } = require('path');
+
+const PATHS = {
+  src: join(__dirname, './src'),
+  public: join(__dirname, './public'),
+  dist: join(__dirname, './dist'),
+};
+
+module.exports = {
+  context: __dirname,
+  devtool: 'eval-source-map',
+  entry: ['react-hot-loader/patch', join(PATHS.src, 'js')],
+  devServer: {
+    port: 9000,
+    hot: true,
+    compress: true,
+    contentBase: PATHS.public,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    stats: 'minimal',
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+  },
+  output: {
+    path: PATHS.dist,
+    filename: 'js/[name].js',
+    chunkFilename: 'js/[name].js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Pokemon Decks',
+      favicon: join(PATHS.public, 'favicon.ico'),
+      template: join(PATHS.public, 'index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: join(PATHS.public, 'index.html'),
+          to: join(PATHS.dist, 'index.html'),
+        },
+      ],
+    }),
+  ],
+};
